eec108b3a94575e448f4a825a4813cc051c1c666
lNektosl <nektos13@yandex.ru>
2024-12-24 16:41:44 +0200

    Moved and Rename projectile Logic

--- Assets/Scripts/ProjectilLogic.cs
+++ Assets/Scripts/ProjectilLogic.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class Swing : MonoBehaviour, IAttack {
+public class ProjectilLogic : MonoBehaviour, IAttack {
     [SerializeField] private float speed;
 
     private Vector2 endPoint;
